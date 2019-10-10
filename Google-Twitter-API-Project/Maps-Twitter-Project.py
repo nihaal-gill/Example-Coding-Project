81dee0839a2cbeeb21762cee6d8196c69bd4e6d4
nihaal-gill <46754086+nihaal-gill@users.noreply.github.com>
2019-10-10 14:10:08 -0500

    Update Maps-Twitter-Project.py

--- Google-Twitter-API-Project/Maps-Twitter-Project.py
+++ Google-Twitter-API-Project/Maps-Twitter-Project.py
@@ -1,7 +1,8 @@
 #Language: Python
 
 #Important Note: I lost access to my Twitter developer API so it does not show the tweets. But if you have a working Google Maps API and 
-#a Twitter developer API you can input those into the program and will as it is said in the description.
+#a Twitter developer API you can input those into the program and will as it is said in the description. You need all files in this folder
+#for the porgram to work.
 
 #Description: This program creates a tkinter-based GUI that enables the user to enter a location and search terms. The search term(s)
 #is then ran through the program 'twitteracess.py' and returns all the tweets in a 2 kilometer radius of the location that the user has 
