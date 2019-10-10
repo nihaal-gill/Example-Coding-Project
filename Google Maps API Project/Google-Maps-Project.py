c245d7552a5ee483c4c2a93d7df3cd2892594932
nihaal-gill <46754086+nihaal-gill@users.noreply.github.com>
2019-10-10 18:04:15 -0500

    Update Google-Maps-Project.py

--- Google Maps API Project/Google-Maps-Project.py
+++ Google Maps API Project/Google-Maps-Project.py
@@ -1,9 +1,3 @@
-#Language: Python
-
-#Description: This project shows my experience of working with the Google API's. The program uses a Tkinter entry widget which allows the user to 
-#input a location that they would like to see on the google map and it brings that location onto the map. As well as it gives the user 
-#the option to view the map between roadmap, satellite, terrain, and hybrid map view. It also allows the user to zoom into their location.
-
 import tkinter
 from tkinter import Tk, Canvas, Frame, Button, Label, Entry, END, LEFT, BOTTOM, TOP, RIGHT, SUNKEN
 import math
